45aaa374e4ae316cb7892567114f91b99b883a44
S-JOBI23-060\student <231122@st.yoshida-g.ac.jp>
2024-06-21 09:47:22 +0900

    完成 #1

--- Card/Program.cs
+++ Card/Program.cs
@@ -8,8 +8,30 @@ namespace Card
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            int number1, number2, number3, number4;
+
+            Console.Write("１つ目のカードの数字を入力してください");
+        
+            number1=int.Parse(Console.ReadLine());
+            
+            Console.Write("２つ目のカードの数字を入力してください");
+        
+            number2=int.Parse(Console.ReadLine());
+            
+            Console.Write("３つ目のカードの数字を入力してください");
+        
+            number3=int.Parse(Console.ReadLine());
+            
+            Console.Write("４つ目のカードの数字を入力してください");
+        
+            number4=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0},{1},{2},{3}",number1,number2,number3,number4);
+
+            Console.ReadLine();
         }
     }
 }
