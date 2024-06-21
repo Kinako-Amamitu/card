4d6e4a16e090cc5b7ee0805d80c98c63f120d232
S-JOBI23-060\student <231122@st.yoshida-g.ac.jp>
2024-06-21 11:39:55 +0900

    修正 #3

--- Card/Program.cs
+++ Card/Program.cs
@@ -11,23 +11,50 @@ namespace Card
 
         static void Main(string[] args)
         {
-            int number1, number2, number3, number4;
+            int number1=0, number2=0, number3=0, number4=0;
 
-            Console.Write("１つ目のカードの数字を入力してください");
-        
-            number1=int.Parse(Console.ReadLine());
             
-            Console.Write("２つ目のカードの数字を入力してください");
-        
-            number2=int.Parse(Console.ReadLine());
-            
-            Console.Write("３つ目のカードの数字を入力してください");
-        
-            number3=int.Parse(Console.ReadLine());
-            
-            Console.Write("４つ目のカードの数字を入力してください");
-        
-            number4=int.Parse(Console.ReadLine());
+            while (number1 <= 0 || number1 > 4)
+            {
+                Console.Write("１つ目のカードの数字を入力してください");
+                number1 = int.Parse(Console.ReadLine());
+                if (number1 <= 0 || number1 > 4)
+                {
+                    Console.WriteLine("もう一度入力してください。");
+                }
+            }
+
+
+
+            while (number2 <= 0 || number2 > 4)
+            {
+                Console.Write("２つ目のカードの数字を入力してください");
+                number2 = int.Parse(Console.ReadLine());
+                if (number2 <= 0 || number2 > 4)
+                {
+                    Console.WriteLine("もう一度入力してください。");
+                }
+            }
+
+            while (number3 <= 0 || number3 > 4)
+            {
+                Console.Write("３つ目のカードの数字を入力してください");
+                number3 = int.Parse(Console.ReadLine());
+                if (number3 <= 0 || number3 > 4)
+                {
+                    Console.WriteLine("もう一度入力してください。");
+                }
+            }
+
+            while (number4 <= 0 || number4 > 4)
+            {
+                Console.Write("４つ目のカードの数字を入力してください");
+                number4 = int.Parse(Console.ReadLine());
+                if (number4 <= 0 || number4 > 4)
+                {
+                    Console.WriteLine("もう一度入力してください。");
+                }
+            }
 
             Console.WriteLine("{0},{1},{2},{3}",number1,number2,number3,number4);
 
